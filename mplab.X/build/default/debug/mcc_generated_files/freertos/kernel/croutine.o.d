0f5f5ed100b5413a81fa7a6a1a67bef1e0f2e7fb
janschop <jan.erik@schopmeier.com>
2024-10-02 12:59:51 +0200

    changed folder namo to .X ending

---
+++ mplab.X/build/default/debug/mcc_generated_files/freertos/kernel/croutine.o.d
@@ -0,0 +1,162 @@
+build/default/debug/mcc_generated_files/freertos/kernel/croutine.o.d \
+ build/default/debug/mcc_generated_files/freertos/kernel/croutine.o: \
+ mcc_generated_files/freertos/kernel/croutine.c \
+ mcc_generated_files/freertos/kernel/include/FreeRTOS.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\stddef.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\stdint.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\bits\alltypes.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\bits\stdint.h \
+ mcc_generated_files/freertos/kernel/include/../../../FreeRTOSConfig.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/system.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/utils/compiler.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\io.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\sfr_defs.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\inttypes.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\features.h \
+ C:/Users/janer/.mchp_packs/Microchip/AVR-Dx_DFP/2.6.303/xc8/avr/include/avr/ioavr64du32.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\portpins.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\common.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\xmega.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\fuse.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\lock.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\builtins.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\xc.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\cci.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\stdbool.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\stdlib.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/utils/interrupt_avr8.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\interrupt.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/config_bits.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/clock.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/ccp.h \
+ c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\utils\compiler.h \
+ c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\protected_io.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/pins.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/./port.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/./utils/compiler.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../timer/tcb0.h \
+ c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\utils\compiler.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../timer/./timer_interface.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../uart/usart1.h \
+ c:\program\ files\microchip\xc8\v2.50\avr\avr\include\stdio.h \
+ c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\system.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../uart/uart_drv_interface.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../uart/uart_types.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../system/interrupt.h \
+ c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\utils\atomic.h \
+ mcc_generated_files/freertos/kernel/include/../../../system/../freertos/freertos_interface.h \
+ mcc_generated_files/freertos/kernel/include/projdefs.h \
+ mcc_generated_files/freertos/kernel/include/portable.h \
+ mcc_generated_files/freertos/kernel/include/deprecated_definitions.h \
+ mcc_generated_files/freertos/kernel/include/../portable/portmacro.h \
+ mcc_generated_files/freertos/kernel/include/mpu_wrappers.h \
+ mcc_generated_files/freertos/kernel/include/task.h \
+ mcc_generated_files/freertos/kernel/include/list.h \
+ mcc_generated_files/freertos/kernel/include/croutine.h
+
+mcc_generated_files/freertos/kernel/include/FreeRTOS.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\stddef.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\stdint.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\bits\alltypes.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\bits\stdint.h:
+
+mcc_generated_files/freertos/kernel/include/../../../FreeRTOSConfig.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/system.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/utils/compiler.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\io.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\sfr_defs.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\inttypes.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\features.h:
+
+C:/Users/janer/.mchp_packs/Microchip/AVR-Dx_DFP/2.6.303/xc8/avr/include/avr/ioavr64du32.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\portpins.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\common.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\xmega.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\fuse.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\lock.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\builtins.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\xc.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\cci.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\lib\gcc\avr\5.4.0\include\stdbool.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\stdlib.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/utils/interrupt_avr8.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\avr\interrupt.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/config_bits.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/clock.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/ccp.h:
+
+c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\utils\compiler.h:
+
+c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\protected_io.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/pins.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/./port.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/./utils/compiler.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../timer/tcb0.h:
+
+c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\utils\compiler.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../timer/./timer_interface.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../uart/usart1.h:
+
+c:\program\ files\microchip\xc8\v2.50\avr\avr\include\stdio.h:
+
+c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\system.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../uart/uart_drv_interface.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../uart/uart_types.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../system/interrupt.h:
+
+c:\users\janer\mplabxprojects\midi.x\mplab.x\mcc_generated_files\system\utils\atomic.h:
+
+mcc_generated_files/freertos/kernel/include/../../../system/../freertos/freertos_interface.h:
+
+mcc_generated_files/freertos/kernel/include/projdefs.h:
+
+mcc_generated_files/freertos/kernel/include/portable.h:
+
+mcc_generated_files/freertos/kernel/include/deprecated_definitions.h:
+
+mcc_generated_files/freertos/kernel/include/../portable/portmacro.h:
+
+mcc_generated_files/freertos/kernel/include/mpu_wrappers.h:
+
+mcc_generated_files/freertos/kernel/include/task.h:
+
+mcc_generated_files/freertos/kernel/include/list.h:
+
+mcc_generated_files/freertos/kernel/include/croutine.h:
