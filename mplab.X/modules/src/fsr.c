69808c8382e70057b49df4667cce38628798ee1a
janschop <jan.erik@schopmeier.com>
2024-12-05 13:29:31 +0100

    removed delays and added pin toggle for timing test

--- mplab.X/modules/src/fsr.c
+++ mplab.X/modules/src/fsr.c
@@ -1,16 +1,19 @@
 #include "../fsr.h"
 #include "../defines.h"
+#include "../../mcc_generated_files/system/pins.h"
 
 uint16_t initial_velocity_counter = 100;
 uint16_t note_off_counter = 100;
 uint8_t threshold = 20;
 
 void play_note(Finger *finger) {
-    DELAY_microseconds(20);
+    // DELAY_microseconds(20);
     ADC0.MUXPOS = finger->fsr_channel; // ADC0_GetConversion(fsr_channel) will "or" not turn off other channels
     uint32_t sample = ADC0_GetConversion(finger->fsr_channel);
     // releasing the adc for other channels to use
-    ADC0.MUXPOS = 0;
+    if (finger->fsr_channel == ADC_MUXPOS_AIN1_gc) IO_PD0_Toggle();
+
+    // ADC0.MUXPOS = 0;
     if (sample >= threshold && !finger->note_on) {
         finger->counter ++;
         // can be used to get max over a period
